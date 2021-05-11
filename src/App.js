3fbf302883e3e10f88caa23b3bef7db1beb5d438
beifuss <esriechtnachwermut@gmail.com>
2021-05-12 00:31:24 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      minNumber: '',
+      maxNumber: '',
+      randomNumber: ''
+    }
+    this.numberHandler = this.numberHandler.bind(this);
+  };
+
+  numberHandler() {
+    let minCurrentNumber = this.state.minNumber;
+    let maxCurrentNumber = this.state.maxNumber;
+    minCurrentNumber = document.getElementById('min_number').value;
+    maxCurrentNumber = document.getElementById('max_number').value
+      
+    this.setState({
+      minNumber: minCurrentNumber,
+      maxNumber: maxCurrentNumber
+    });
+    
+    let random = minCurrentNumber - 0.5 + Math.random() * (maxCurrentNumber - minCurrentNumber + 1);
+    random = Math.round(random);
+
+    this.setState({
+      randomNumber: random
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="box">
+          <div className="output">
+            Random number is: {this.state.randomNumber}
+          </div>
+          <div className="input">
+            <input id="min_number" type="text" placeholder="min:" />
+            <input id="max_number" type="text" placeholder="max:" />
+          </div>
+            
+            <button id="generate" onClick={this.numberHandler}>Generate</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
